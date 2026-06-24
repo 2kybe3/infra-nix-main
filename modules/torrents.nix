e2e2bfa0ecf7f7bf3b4ee531a97c4f7b8688fcb1
2kybe3 <kybe@kybe.xyz>
2026-06-24 12:02:45 +0200

    update

--- modules/torrents.nix
+++ modules/torrents.nix
@@ -19,8 +19,8 @@ let
   wgInterface = "mullvad0";
   wgDns = "10.64.0.1";
   wgIps = [
-    "10.71.204.152/32"
-    "fc00:bbbb:bbbb:bb01::8:cc97/128"
+    "10.69.216.71/32"
+    "fc00:bbbb:bbbb:bb01::6:d846/128"
   ];
   wgEndpoint = "193.32.248.66:51820";
   # noble cub
