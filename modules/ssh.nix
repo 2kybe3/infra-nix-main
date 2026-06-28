db30cd58aaf6524d21b530e1060ae43f55cfb1f1
2kybe3 <kybe@kybe.xyz>
2026-06-28 16:08:02 +0200

    infra deployer

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -10,6 +10,7 @@
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM7irWuDZwx7ZvPSiUwBbxUxKL/7aMQmy/8oxput1bID kybe@knx"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/cSa7NMgCbuAOI7Nw1nP5RaysLGBpEthFtfPvL+2vR infra deployer"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOQl0E3yt31laA6LoeQcXoUCbmjDBi/qH6E/IlC/lMtF nix-builder -> nix-main"
   ];
 }
